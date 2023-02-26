4708174a526273ea72f705b8eb0a7ff2090817db
Oğuzhan Bolat <oguzhanbolat0@gmail.com>
2023-02-26 22:16:48 +0300

    CQRS

---
+++ Onion/Core/Application/DTO/ActorDTOModel/UpdateActorDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Application.DTO
+{
+	public class UpdateActorDTO
+	{
+        public string ActorName { get; set; }
+        public string ActorSurname { get; set; }
+    }
+}
+
